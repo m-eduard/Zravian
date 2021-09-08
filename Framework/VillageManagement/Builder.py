75e38dfd124d76053114c27bd3537cd588ac6fb3
AlexDeVis <alexx.apostolescu@gmail.com>
2021-09-08 14:06:19 +0300

    Builder smoke test works

--- Framework/VillageManagement/Builder.py
+++ Framework/VillageManagement/Builder.py
@@ -35,16 +35,16 @@ def get_busy_workers_timer(sws : SWS):
                     workingTimer = sws.getElementAttribute(propList, 'text')
                     time.sleep(1)
             if not sws.get(initialURL):
-                logger.error('In function get_busy_workers_timer: Failed to return to initial URL')
+                logger.error('In get_busy_workers_timer: Failed to return to initial URL')
             else:
                 if workingTimer:
                     ret = time_to_seconds(workingTimer[0])
                 else:
                     ret = 0
         else:
-            logger.error('In function get_busy_workers_timer: Failed to move to overview')
+            logger.error('In get_busy_workers_timer: Failed to move to overview')
     else:
-        logger.error('In function get_busy_workers_timer: Failed to get the current URL')
+        logger.error('In get_busy_workers_timer: Failed to get the current URL')
     return ret
 
 
@@ -68,7 +68,7 @@ def get_time_to_build(sws : SWS, bdType):
                 costs = costsTag[0]
                 time_left = time_to_seconds(costs.split('|')[-1])
             else:
-                logger.error('In function get_time_to_build: Could not find costs')
+                logger.error('In get_time_to_build: Could not find construct costs')
         else:
             propList = [XPATH.LEVEL_UP_COSTS]
             costsTag = sws.getElementAttribute(propList, 'text')
@@ -76,9 +76,9 @@ def get_time_to_build(sws : SWS, bdType):
                 costs = costsTag[0]
                 time_left = time_to_seconds(costs.split('|')[-1].split()[0])
             else:
-                logger.error('In function get_time_to_build: Could not find costs')
+                logger.error('In get_time_to_build: Could not find upgrade costs')
     else:
-        logger.error('In function get_time_to_build: Invalid parameter bdType')
+        logger.error('In get_time_to_build: Invalid parameter bdType')
     return time_left
 
 
@@ -107,17 +107,17 @@ def press_upgrade_button(sws : SWS, bdType, waitToFinish=False):
             if sws.clickElement(propList, refresh=True):
                 if waitToFinish:
                     if sws.get(initialURL):
-                        logger.info('In function press_upgrade_button: Sleep for %d seconds' % time_to_build)
+                        logger.info('In press_upgrade_button: Sleep for %d seconds' % time_to_build)
                         time.sleep(time_to_build)
                     else:
-                        logger.error('In function press_upgrade_button: Failed to return to initial URL')
+                        logger.error('In press_upgrade_button: Failed to return to initial URL')
                 status = True
             else:
-                logger.error('In function press_upgrade_button: Unable to press construct / upgrade button')
+                logger.error('In press_upgrade_button: Unable to press construct / upgrade button')
         else:
-            logger.error('In function press_upgrade_button: Failed to get current url')
+            logger.error('In press_upgrade_button: Failed to get current url')
     else:
-        logger.error('In function press_upgrade_button: Invalid parameter bdType')
+        logger.error('In press_upgrade_button: Invalid parameter bdType')
     return status
 
 
@@ -145,13 +145,13 @@ def select_and_demolish_building(sws : SWS, index):
                         dmTime = max(1, time_to_seconds(demolitionTimer[0]))
                         time.sleep(dmTime)
                 status = True
-                logger.success(f'In function select_and_demolish_building: Successfully demolished {index}')
+                logger.success(f'In select_and_demolish_building: Successfully demolished {index}')
             else:
-                logger.error('In function select_and_demolish_building: Failed to press demolition button')
+                logger.error('In select_and_demolish_building: Failed to press demolition button')
         else:
-            logger.error(f'In function select_and_demolish_building: Could not select option {index}')
+            logger.error(f'In select_and_demolish_building: Could not select option {index}')
     else:
-        logger.error('In function select_and_demolish_building: Main building page not visible')
+        logger.error('In select_and_demolish_building: Main building page not visible')
     return status
 
 
@@ -176,10 +176,10 @@ def check_requirements(sws : SWS, bdType, forced=False):
             if not reqBdList:  # Construct
                 if forced:
                     if not construct_building(sws, reqBd, forced=True, waitToFinish=True):
-                        logger.error(f'In function check_requirements: Failed to construct {reqBd}')
+                        logger.error(f'In check_requirements: Failed to construct {reqBd}')
                         break
                 else:
-                    logger.warning(f'In function check_requirements: {reqBd} not found')
+                    logger.warning(f'In check_requirements: {reqBd} not found')
                     break
             reqBdList = get_building_data(sws, reqBd)
             # Check level
@@ -187,16 +187,16 @@ def check_requirements(sws : SWS, bdType, forced=False):
                 if forced:
                     while reqBdList[-1][1] < reqLevel:
                         if not level_up_building_at(sws, reqBdList[-1][0], forced=True, waitToFinish=True):
-                            logger.error(f'In function check_requirements: Failed to level up {reqBd}')
+                            logger.error(f'In check_requirements: Failed to level up {reqBd}')
                             break
                         reqBdList = get_building_data(sws, reqBd)
                 else:
-                    logger.warning(f'In function check_requirements: {reqBd} level is too low')
+                    logger.warning(f'In check_requirements: {reqBd} level is too low')
                     break
         else:  # If not break encountered, all requirements are fulfilled
             status = True
     else:
-        logger.error('In function check_requirements: Invalid parameter bdType')
+        logger.error('In check_requirements: Invalid parameter bdType')
     return status
 
 
@@ -226,22 +226,22 @@ def check_storage(sws : SWS, bdType, storageType, forced=False):
                     storageList = get_building_data(sws, storageType)
                     if not storageList:
                         if not construct_building(sws, storageType, forced=True, waitToFinish=True):
-                            logger.error(f'In function check_storage: Failed to construct {storageType}')
+                            logger.error(f'In check_storage: Failed to construct {storageType}')
                         else:  # Recheck storage
                             status = check_storage(sws, bdType, storageType, forced=True)
                     else:
                         if not level_up_building_at(sws, storageList[-1][0], forced=True, waitToFinish=True):
-                            logger.error(f'In function check_storage: Failed to level up {storageType}')
+                            logger.error(f'In check_storage: Failed to level up {storageType}')
                         else:  # Recheck storage
                             status = check_storage(sws, bdType, storageType, forced=True)
                 else:
-                    logger.warning(f'In function check_storage: {storageType} not big enough')
+                    logger.warning(f'In check_storage: {storageType} not big enough')
             else:
                 status = True
         else:
-            logger.error('In function check_storage: Invalid parameter storageType')
+            logger.error('In check_storage: Invalid parameter storageType')
     else:
-        logger.error('In function check_storage: Invalid parameter bdType')
+        logger.error('In check_storage: Invalid parameter bdType')
     return status
 
 
@@ -272,13 +272,13 @@ def check_resources(sws : SWS, bdType, forced=False):
                 if sws.refresh():
                     status = check_resources(sws, bdType, forced=True)
                 else:
-                    logger.error('In function check_resources: Failed to refresh page')
+                    logger.error('In check_resources: Failed to refresh page')
             else:
-                logger.warning('In function check_resources: Not enough resources')
+                logger.warning('In check_resources: Not enough resources')
         else:
             status = True
     else:
-        logger.error('In function check_resources: Invalid parameter bdType')
+        logger.error('In check_resources: Invalid parameter bdType')
     return status
 
 
@@ -303,13 +303,13 @@ def check_busy_workers(sws : SWS, bdType, forced=False):
                 if sws.refresh():
                     status = check_busy_workers(sws, bdType, forced=True)
                 else:
-                    logger.error('In function check_busy_workers: Failed to refresh page')
+                    logger.error('In check_busy_workers: Failed to refresh page')
             else:
-                logger.info('In function check_busy_workers: Workers are busy')
+                logger.info('In check_busy_workers: Workers are busy')
         else:
             status = True
     else:
-        logger.error('In function check_busy_workers: Invalid parameter bdType')
+        logger.error('In check_busy_workers: Invalid parameter bdType')
     return status
 
 
@@ -327,11 +327,11 @@ def check_not_max_level(sws : SWS, bdType):
     status = False
     if isinstance(bdType, BuildingType):
         if sws.isVisible(XPATH.BUILDING_ERR_MAX_LVL):
-            logger.info(f'In function check_not_max_level: {bdType} is at max level')
+            logger.info(f'In check_not_max_level: {bdType} is at max level')
         else:
             status = True
     else:
-        logger.error('In function check_not_max_level: Invalid parameter bdType')
+        logger.error('In check_not_max_level: Invalid parameter bdType')
     return status
 
 
@@ -355,7 +355,7 @@ def construct_building(sws : SWS, bdType, forced=False, waitToFinish=False):
     if isinstance(bdType, BuildingType):
         if bdType in ResourceFields:
             status = True  # Resource fields are already constructed
-            logger.info(f'In function construct_building: {BUILDINGS[bdType].name} is in resource fields')
+            logger.info(f'In construct_building: {BUILDINGS[bdType].name} is in resource fields')
         else:
             if move_to_village(sws):
                 if check_requirements(sws, bdType, forced):
@@ -379,22 +379,22 @@ def construct_building(sws : SWS, bdType, forced=False, waitToFinish=False):
                                         logger.success('Success building %s' % BUILDINGS[bdType].name)
                                         status = True
                                     else:
-                                        logger.error('In function construct_building: Failed to press\
+                                        logger.error('In construct_building: Failed to press\
                                             upgrade button')
                                 else:
-                                    logger.error('In function construct_building: Storagecheck failed')
+                                    logger.error('In construct_building: Storagecheck failed')
                             else:
-                                logger.error('In function construct_building: Entering empty building site failed')
+                                logger.error('In construct_building: Entering empty building site failed')
                         else:
-                            logger.error('In function construct_building: Village is full')
+                            logger.error('In construct_building: Village is full')
                     else:
-                        logger.info(f'In function construct_building: {BUILDINGS[bdType].name} is already constructed')
+                        logger.info(f'In construct_building: {BUILDINGS[bdType].name} is already constructed')
                 else:
-                    logger.error('In function construct_building: Requirements check failed')
+                    logger.error('In construct_building: Requirements check failed')
             else:
-                logger.error('In function construct_building: Failed to move to village')
+                logger.error('In construct_building: Failed to move to village')
     else:
-        logger.error('In function construct_building: Invalid parameter bdType')
+        logger.error('In construct_building: Invalid parameter bdType')
     return status
 
 
@@ -427,9 +427,9 @@ def level_up_building_at(sws, index, forced=False, waitToFinish=False):
                     buildingSiteName = buildingSiteRe.group()[:-len('level')]
                     bdType = get_building_type_by_name(buildingSiteName)
                 else:
-                    logger.error(f'In function level_up_building_at: Element "alt" tag does not respect pattern')
+                    logger.error(f'In level_up_building_at: Element "alt" tag does not respect pattern')
             else:
-                logger.error(f'In function level_up_building_at: Element not found')
+                logger.error(f'In level_up_building_at: Element not found')
             if bdType:
                 logger.info('Attempting to construct %s' % BUILDINGS[bdType].name)
                 if enter_building_menu(sws, index):
@@ -444,17 +444,17 @@ def level_up_building_at(sws, index, forced=False, waitToFinish=False):
                                 logger.success('Success leveling up %s' % BUILDINGS[bdType].name)
                                 status = True
                             else:
-                                logger.error('In function level_up_building_at: Failed to press upgrade button')
+                                logger.error('In level_up_building_at: Failed to press upgrade button')
                     else:
-                        logger.error(f'In function level_up_building_at: Building site at {index} is empty')
+                        logger.error(f'In level_up_building_at: Building site at {index} is empty')
                 else:
-                    logger.error(f'In function level_up_building_at: Failed to enter building menu {index}')
+                    logger.error(f'In level_up_building_at: Failed to enter building menu {index}')
             else:
-                logger.error(f'In function level_up_building_at: Error identifying building type')
+                logger.error(f'In level_up_building_at: Error identifying building type')
         else:
-            logger.error('In function level_up_building_at: Failed to change view')
+            logger.error('In level_up_building_at: Failed to change view')
     else:
-        logger.error(f'In function level_up_building_at: Invalid index {index}')
+        logger.error(f'In level_up_building_at: Invalid index {index}')
     return status
 
 
@@ -479,24 +479,27 @@ def demolish_building_at(sws : SWS, index):
     if mainBuildingIndex:
         if enter_building_menu(sws, mainBuildingIndex) and \
                 check_building_page_title(sws, BuildingType.MainBuilding):
-            for index in wrapper:
-                if index >= FIRST_BUILDING_SITE_VILLAGE and index < LAST_BUILDING_SITE_VILLAGE:
-                    if mainBuildingIndex == index:
-                        continue
-                    if not select_and_demolish_building(sws, index):
-                        logger.error(f'In function demolish_building: Failed to select/demolish')
+            if sws.isVisible(XPATH.DEMOLITION_BTN):
+                for index in wrapper:
+                    if index >= FIRST_BUILDING_SITE_VILLAGE and index < LAST_BUILDING_SITE_VILLAGE:
+                        if mainBuildingIndex == index:
+                            continue
+                        if not select_and_demolish_building(sws, index):
+                            logger.error(f'In demolish_building: Failed to select/demolish')
+                            break
+                    else:
+                        logger.error(f'In demolish_building: Invalid index value {index}')
                         break
                 else:
-                    logger.error(f'In function demolish_building: Invalid index value {index}')
-                    break
+                    status = True
             else:
-                status = True
+                logger.warning('In demolish_building: Level up Main Building first')
         else:
-            logger.error('In function demolish_building: Failed to enter main building')
+            logger.error('In demolish_building: Failed to enter main building')
     else:
-        logger.warning('In function demolish_building: Main building not found')
+        logger.warning('In demolish_building: Main building not found')
     if not move_to_village(sws):
-        logger.error('In function demolish_building: Failed to move to village')
+        logger.error('In demolish_building: Failed to move to village')
     else:
         print('Moved to village')
     return status
