b4f9d3fb3f0bc8542bb257894b528d01aa0802da
AlexDeVis <alexx.apostolescu@gmail.com>
2021-09-10 20:43:08 +0300

    Updated req_acc_1

--- testing/tests_account/req_acc_1.py
+++ testing/tests_account/req_acc_1.py
@@ -1,5 +1,5 @@
-from Framework.account.AccountLibraryManager import JSON_PASSWORD_KEY, JSON_USERNAME_KEY, get_account_library, get_random_account
 import unittest
+from Framework.account.AccountLibraryManager import JSON_PASSWORD_KEY, JSON_USERNAME_KEY, get_account_library, get_random_account
 from Framework.utility.Constants import Server
 from Framework.utility.Logger import get_projectLogger
 from Framework.account.CreateAcoount import create_new_account
@@ -16,28 +16,22 @@ class Test_AccountCreation(unittest.TestCase):
         self.assertIsNotNone(get_account_library())
 
     def test_create_account(self):
-        # Step 1: Read account_library.json before account creation
         initialJson = get_account_library()
         self.assertIsNotNone(initialJson)
-        # Step 2: Create a default account on each server
         for sv in Server:
             self.assertTrue(create_new_account(server=sv))
-        # Step 3: Read account_library.json after account creation
         currentJson = get_account_library()
         self.assertIsNotNone(currentJson)
-        # Step 4: Check differences
         for sv in Server:
             self.assertTrue(len(currentJson[str(sv.value)]) == len(initialJson[str(sv.value)]) + 1)
 
     def test_create_account_dup_name(self):
-        # Step 1: Find existing account
         credentials = None
         for sv in Server:
             credentials = get_random_account(sv)
             if credentials:
                 break
         self.assertIsNotNone(credentials)
-        # Step 2: Attempt to create identical account
         self.assertFalse(create_new_account(credentials[JSON_USERNAME_KEY], credentials[JSON_PASSWORD_KEY]))
         
 
