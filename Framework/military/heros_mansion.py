a6218c0d0b0c1d7c299d8c7df4493069feee45e8
m-eduard <marineduardconstantin2001@gmail.com>
2021-10-01 00:00:39 +0300

    New function for hero name change

--- Framework/military/heros_mansion.py
+++ Framework/military/heros_mansion.py
@@ -1,6 +1,7 @@
 from Framework.infrastructure.builder import check_building_page_title
 from Framework.utility.Constants import  BuildingType, TroopType, get_TROOPS, get_XPATH, get_projectLogger
-from Framework.utility.SeleniumWebScraper import SWS
+from Framework.utility.SeleniumWebScraper import SWS, Attr
+from selenium.webdriver.common.keys import Keys
 
 logger = get_projectLogger()
 TROOPS = get_TROOPS()
@@ -30,4 +31,34 @@ def train_hero(sws : SWS, tpType : TroopType):
     else:
         logger.error("In function train_hero: Not Hero's Mansion screen")
     
+    return status
+
+def name_hero(sws : SWS, newName : str):
+    """
+    Gives a name to the hero. (should be called after train_hero)
+
+    Parameters:
+        - sws (SWS): Selenium Web Scraper.
+        - newName (str): Denotes hero name.
+
+    Returns:
+        - True if the operation is successful, False otherwise.
+    """
+    status = False
+
+    oldName = sws.getElementAttribute(XPATH.HERO_NAME, Attr.VALUE)
+    if oldName:
+        for i in range(len(oldName)):
+            sws.sendKeys(XPATH.HERO_NAME, Keys.BACKSPACE)
+
+    if sws.sendKeys(XPATH.HERO_NAME, newName):
+        logger.success(f'In function name_hero: The hero is named now "{newName}".')
+        if sws.clickElement(XPATH.HERO_SAVE_NAME):
+            status = True
+            logger.success('In function name_hero: The hero name is now saved.')
+        else:
+            logger.error('In function name_hero: Couldn\'t push save name button.')
+    else:
+        logger.error('In function name_hero: Couldn\'t send the new hero name in the textbox.')
+
     return status
