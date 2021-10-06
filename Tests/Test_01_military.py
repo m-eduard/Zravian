2e787d6b32a727263fe688a77a77e19a7d300135
m-eduard <marineduardconstantin2001@gmail.com>
2021-10-06 16:54:28 +0300

    New tests for upgrade troops and change hero name

--- Tests/Test_01_military.py
+++ Tests/Test_01_military.py
@@ -10,6 +10,9 @@ from Framework.infrastructure.builder import enter_building
 from Framework.utility.Constants import BuildingType, Server, TroopType
 from Framework.account.Login import XPATH, login
 from Framework.military.troops_trainer import make_troops_by_amount, reduce_train_time, troop_max_amount
+from Framework.military.upgrade_troops import upgrade_troop_defense, upgrade_troop_offense
+from Framework.military.heros_mansion import name_hero, train_hero
+
 
 # def setup_module():
 # 	global sws
@@ -65,17 +68,55 @@ class Test_01_military:
 
 					#S4: Sometimes, go in other building than the one where you can train troops
 					if i % 2 == 0:
-						enter_building(sws, BuildingType.Brewery)
+						enter_building(sws, BuildingType.MainBuilding)
 
 					#S5: Call make_troops_by_amount() for every troop.
 					#O5: make_troops_by_amount() should return only True
-					assert make_troops_by_amount(sws, get_tpType_by_name(troops[i]), int(max % 1000))
+					assert make_troops_by_amount(sws, get_tpType_by_name(troops[i]), max % 1000)
 
 	# reduce train time button should be available in order to run successfully this test
 	def test_01_military_02(self):
 		"""
 		Id: 03
 		Description: Test reduce_train_time()
+		Steps:
+			1. Start from an initial phase (no troops are training).
+			2. Call make_troops_by_amount() for every building that supports training troops.
+			3. Call reduce_train_time() before leaving a building.		
+			4. Check if training troops table exists.
+		Objectives:
+			4. After calling reduce_train_time(), if was successful, then no troops should be currently training (so no table).
+		"""
+		with login(Server.S10k, username="0bomb15", headless=True) as sws:
+			buildings = [BuildingType.Barracks, BuildingType.Stable, BuildingType.SiegeWorkshop]
+
+			#S1: Wait for the initial phase.
+			for bdType in buildings:
+				enter_building(sws, bdType)
+				while sws.isVisible('//*[@class="under_progress"]'):
+					time.sleep(15)
+
+			for bdType in buildings:
+				assert enter_building(sws, bdType)
+				troops = sws.getElementsAttribute('//table[@class="build_details"]//*[@class="tit"]//a', Attr.TEXT)
+
+				for i in troops:
+					max = troop_max_amount(sws, get_tpType_by_name(i))
+
+					#S2: Call make_troops_by_amount() for every troop.
+					assert make_troops_by_amount(sws, get_tpType_by_name(i), max % 1000)
+
+
+				#S3: Call reduce_train_time() before leaving a building.
+				if reduce_train_time(sws, bdType) is True:
+					#S4: Check if training troops table exists.
+					#O4: No training troops tables should be visible
+					assert sws.isVisible('//*[@class="under_progress"]') == False
+
+	def test_01_military_03(self):
+		"""
+		Id: 03
+		Description: Test upgrade_troop_defense() / upgrade_troop_offense()
 		Steps:
 			1. Start from an initial phase (no troops are training).
 			2. Call make_troops_by_amount() for every building that supports training troops.
@@ -109,3 +150,90 @@ class Test_01_military:
 					#S4: Check if training troops table exists.
 					#O4: No training troops tables should be visible
 					assert sws.isVisible('//*[@class="under_progress"]') == False
+
+	def test_01_military_03(self):
+		"""
+		Id: 03
+		Description: Test upgrade_troop_defense() / upgrade_troop_offense()
+		Steps:
+			1. Get the current level for the first troop that supports upgrade
+			2. Call the forespoken functions for the selected troop
+		Objectives:
+			2. The level should be increased, if the action was possible
+		"""
+		with login(Server.S10k, username="0bomb15", headless=True) as sws:
+			ls = ['defense', 'offense']
+			for upgradeType in ls:
+				test = upgradeType
+
+				if test == 'offense':
+					assert enter_building(sws, BuildingType.Blacksmith)
+				else:
+					assert enter_building(sws, BuildingType.Armoury)
+
+				troopsStatus = sws.getElementsAttribute('//*[@class="build_details"]//tbody//*[@class="act"]', Attr.TEXT)
+				troopsName = sws.getElementsAttribute('//*[@class="build_details"]//tbody//*[@class="desc"]//a', Attr.TEXT)
+
+				# S1: Get the current level for the first troop that supports upgrade
+				index = -1
+				for i in range(len(troopsStatus)):
+					if troopsStatus[i] == 'Upgrade':
+						index = i
+						break
+
+				if index != -1:
+					troop = get_tpType_by_name(troopsName[index])
+					currentLevel = sws.getElementAttribute(f'//*[@class="build_details"]\
+															//tr[{index + 1}]//*[@class="info"]', Attr.TEXT)
+					currentLevel = currentLevel.split()
+					currentLevel = int(currentLevel[-1][:-1])
+
+					notLast = True
+					if currentLevel == 19:
+						notLast = False
+
+					# S2: Call the tested functions, one at a time
+					if test == 'offense':
+						assert upgrade_troop_offense(sws, troop, True)
+					else:
+						assert upgrade_troop_defense(sws, troop, True)
+
+					# O2: Check if the troop's level was increased
+					if notLast:
+						newLevel = sws.getElementAttribute(f'//*[@class="build_details"]\
+																//tr[{index + 1}]//*[@class="info"]', Attr.TEXT)
+						newLevel = newLevel.split()
+						newLevel = int(newLevel[-1][:-1])
+						assert newLevel - currentLevel == 1
+					else:
+						size = len(sws.getElementsAttribute('//*[@class="build_details"]//tbody//*[@class="act"]', Attr.TEXT))
+						assert len(troopsStatus) - size == 1
+
+		
+	def test_01_military_04(self):
+		"""
+		Id: 04
+		Description: Test name_hero()
+		Steps:
+			1. Call the name_hero() using a random string as the new name.
+			2. Refresh the page and get the current hero name.
+		Objectives:
+			2. The new hero name should match the random string sent as parameter
+			   to name_hero()
+		"""
+		with login(Server.S10k, username="0bomb15", headless=True) as sws:
+			assert enter_building(sws, BuildingType.HeroMansion)
+			train_hero(sws, TroopType.Clubswinger)
+
+			# S1: Call the name_hero() using a random string as the new name
+			generatedName = str(time.time())
+			generatedName = generatedName[:20]
+			assert name_hero(sws, generatedName)
+
+			# S2: Refresh the page and get the current hero name.
+			sws.refresh()
+			newName = sws.getElementAttribute(XPATH.HERO_NAME, Attr.VALUE)
+
+			# O2: The new hero name should match the random string sent as parameter
+			#     to name_hero()
+			assert newName == generatedName
