ccd647f9f5dc8d6de6cfb6a95f28bea75b1a4d34
m-eduard <marineduardconstantin2001@gmail.com>
2021-10-06 16:52:25 +0300

    updated imports

--- Framework/military/heros_mansion.py
+++ Framework/military/heros_mansion.py
@@ -1,7 +1,6 @@
 from Framework.infrastructure.builder import check_building_page_title
-from Framework.utility.Constants import  BuildingType, TroopType, get_TROOPS, get_XPATH, get_projectLogger
-from Framework.utility.SeleniumWebScraper import SWS, Attr
-from selenium.webdriver.common.keys import Keys
+from Framework.utility.Constants import BuildingType, TroopType, get_TROOPS, get_XPATH, get_projectLogger
+from Framework.utility.SeleniumWebScraper import SWS
 
 logger = get_projectLogger()
 TROOPS = get_TROOPS()
@@ -30,8 +29,8 @@ def train_hero(sws : SWS, tpType : TroopType):
             logger.warning("In function train_hero: Can't train another hero as there is already one existing.")
     else:
         logger.error("In function train_hero: Not Hero's Mansion screen")
-    
     return status
+
 
 def name_hero(sws : SWS, newName : str):
     """
@@ -45,20 +44,16 @@ def name_hero(sws : SWS, newName : str):
         - True if the operation is successful, False otherwise.
     """
     status = False
-
-    oldName = sws.getElementAttribute(XPATH.HERO_NAME, Attr.VALUE)
-    if oldName:
-        for i in range(len(oldName)):
-            sws.sendKeys(XPATH.HERO_NAME, Keys.BACKSPACE)
-
-    if sws.sendKeys(XPATH.HERO_NAME, newName):
-        logger.success(f'In function name_hero: The hero is named now "{newName}".')
-        if sws.clickElement(XPATH.HERO_SAVE_NAME):
-            status = True
-            logger.success('In function name_hero: The hero name is now saved.')
+    if sws.sendKeys(XPATH.HERO_NAME, None, waitFor=True):
+        if sws.sendKeys(XPATH.HERO_NAME, newName):
+            logger.success(f'In name_hero: The hero is named now "{newName}".')
+            if sws.clickElement(XPATH.HERO_SAVE_NAME):
+                status = True
+                logger.success('In name_hero: The hero name is now saved.')
+            else:
+                logger.error('In name_hero: Couldn\'t push save name button.')
         else:
-            logger.error('In function name_hero: Couldn\'t push save name button.')
+            logger.error('In name_hero: Couldn\'t send the new hero name in the textbox.')
     else:
-        logger.error('In function name_hero: Couldn\'t send the new hero name in the textbox.')
-
+        logger.error('In name_hero: Couldn\'t send the new hero name in the textbox.')
     return status
--- Framework/military/upgrade_troops.py
+++ Framework/military/upgrade_troops.py
@@ -1,5 +1,5 @@
 from Framework.infrastructure.builder import enter_building
-from Framework.utility.Constants import  BuildingType, TroopType, get_TROOPS, get_XPATH, get_projectLogger
+from Framework.utility.Constants import  BuildingType, TroopType, get_TROOPS, get_XPATH, get_projectLogger, time_to_seconds
 from Framework.utility.SeleniumWebScraper import SWS, Attr
 from time import sleep
 
@@ -7,31 +7,75 @@ logger = get_projectLogger()
 TROOPS = get_TROOPS()
 XPATH = get_XPATH()
 
-def upgrade_troop(sws : SWS, tpType : TroopType, bdType : BuildingType):
+def upgrade_troop_offense(sws : SWS, tpType : TroopType, waitFor : bool = False):
 	"""
-	Upgrade the speciefied troop inside the specified building
+	Upgrade the specified troop's offense.
 
 	Parameters:
 		- sws (SWS): Selenium Web Scraper.
 		- tpType (TroopType): Denotes troop.
-		- bdType (BuildingType): Denotes the building where you want to upgrade the troop.
+		- waitFor (bool): If it's True, it waits until the requested troop upgrade is done;
+						  else, after sending the upgrade signal, the function ends
 
 	Returns:
 		- True if the operation is successful, False otherwise.
 	"""
-	status = False
-
-	target_buildings = [BuildingType.Armoury, BuildingType.Blacksmith]
-
-	if bdType in target_buildings:
-		enter_building(sws, bdType)
-
-		if sws.clickElement(XPATH.UPGRADE_BTN % TROOPS[tpType].name, refresh=True):
-			logger.success(f'In function upgrade_troop: {TROOPS[tpType].name} was upgraded')
-			status = True
-		else:
-			logger.error('In function upgrade_troop: Failed to press upgrade')
-	else:
-		logger.error('In function upgrade_troop: Received an invalid bdType parameter')
-	
+	status = __upgrade_troop(sws, tpType, BuildingType.Blacksmith, 'upgrade_troop_offense')
+	if waitFor:
+		sleep(get_update_time(sws, tpType) + 0.1)
 	return status
+
+
+def upgrade_troop_defense(sws : SWS, tpType : TroopType, waitFor : bool = False):
+	"""
+	Upgrade the specified troop's defense.
+
+	Parameters:
+		- sws (SWS): Selenium Web Scraper.
+		- tpType (TroopType): Denotes troop.
+		- waitFor (bool): If it's True, it waits until the requested troop upgrade is done;
+						  else, after sending the upgrade signal, the function ends
+
+	Returns:
+		- True if the operation is successful, False otherwise.
+	"""
+	status = __upgrade_troop(sws, tpType, BuildingType.Armoury, 'upgrade_troop_defense')
+	if waitFor:
+		sleep(get_update_time(sws, tpType) + 0.1)
+	return status
+
+
+def __upgrade_troop(sws : SWS, tpType : TroopType, bdType : BuildingType, functionName : str):
+	status = False
+	target_buildings = [BuildingType.Armoury, BuildingType.Blacksmith]
+	if bdType in target_buildings:
+		if enter_building(sws, bdType):
+			if sws.clickElement(XPATH.UPGRADE_BTN % TROOPS[tpType].name, refresh=True):
+				logger.success(f'In function {functionName}: {TROOPS[tpType].name} was upgraded')
+				status = True
+			else:
+				logger.error(f'In function {functionName}: Failed to press upgrade')
+		else:
+			logger.error(f'In {functionName}: Failed to enter {bdType}')
+	else:
+		logger.error(f'In function {functionName}: Received an invalid bdType parameter')
+	return status
+
+
+def get_update_time(sws : SWS, tpType : TroopType):
+	"""
+	Get the time until the specified troop is upgraded.
+
+	Parameters:
+		- sws (SWS): Selenium Web Scraper.
+		- tpType (TroopType): Denotes troop.
+
+	Returns:
+		- time in seconds
+	"""
+	time = sws.getElementAttribute(XPATH.UPGRADE_TIME % TROOPS[tpType].name, Attr.TEXT)
+	if time == None or time[-1] == '?':
+		time = 0
+	else:
+		time = time_to_seconds(time)
+	return time
